b4016fdbd9278f56d3930a80d6755fa58d75360a
Isaam Saeed <isaam@persuit.com>
2021-01-12 17:32:10 +1100

    i3 config

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@
 # Set up symlinks
 ln -sv /home/$USER/dotfiles/.vimrc /home/$USER/.vimrc
 ln -sv /home/$USER/dotfiles/.tmux.conf /home/$USER/.tmux.conf
+ln -sv /home/$USER/dotfiles/i3config /home/$USER/.config/i3/config
