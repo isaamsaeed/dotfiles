689f2da47b10dab22cb74eb1c9e72d29c909541c
Isaam Saeed <isaam@persuit.com>
2022-06-15 16:16:13 +1000

    Update setup script to backup any existing config

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dotfiles
+
+Config that I use for i3, zsh, tmux and vim (my setup is pretty minimal and doesn't deviate that much from defaults)
--- setup.sh
+++ setup.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Back up any existing config
+if [ -f "/home/$USER/.vimrc" ]; then
+	mv /home/$USER/.vimrc{,.bak}
+fi
+
+if [ -f "/home/$USER/.tmux.conf" ]; then
+	mv /home/$USER/.tmux.conf{,.bak}
+fi
+
+if [ -f "/home/$USER/.config/i3/config" ]; then
+	mv /home/$USER/.config/i3/config{,.bak}
+else
+	# Set up config directory for i3
+	mkdir -p /home/$USER/.config/i3
+fi
+
 # Set up symlinks
 ln -sv /home/$USER/dotfiles/.vimrc /home/$USER/.vimrc
 ln -sv /home/$USER/dotfiles/.tmux.conf /home/$USER/.tmux.conf
